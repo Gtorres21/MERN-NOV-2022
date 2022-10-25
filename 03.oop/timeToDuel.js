22c989a300b6365e48348b049386d98c1590716f
Gtorres21 <giovannytorres21@gmail.com>
2022-10-25 12:54:51 -0700

    Completed Assignment Time  To Duel

--- 03.oop/timeToDuel.js
+++ 03.oop/timeToDuel.js
@@ -15,7 +15,11 @@ class Unit extends Card{
     }
 
     attack(target){
-        return this.res-target.power;
+        if(target instanceof Unit){
+            return console.log(this.name + " attacked "+ target.name + " for " + (this.res-target.power) );
+        }else{
+            return "Target must be a Unit!";
+        }
     }
 
 
@@ -23,24 +27,45 @@ class Unit extends Card{
 
 
 class Effect extends Card{
-    constructor(name, cost){
+    constructor(name, cost,stat,magnitude, text){
         super(name, cost);
+        this.stat = stat;
+        this.magnitude = magnitude;
+        this.text = text;
 
     }
-    raiseRes(target){
-        return target.res += 3;
+    play(target){
+        if(target instanceof Unit){
+            if(this.stat="resilience"){
+                target.res+=this.magnitude;
+                return console.log(this.text);
+            }else{
+                target.power+=this.magnitude;
+                return console.log(this.text);
+            }
+        }else{
+            return "Target must be a Unit!";
+        }
     }
-    lowerRes(target){
-        return target.res -= 3;
-    }
-    raisePower(target){
-        return target.power +=3;
-    }
-    lowerPower(target){
-        return target.power -=3;
-    }
+
+
+
 
 }
+
+const RedBeltNinja = new Unit("Red Belt Ninja", 3,3,4);
+const BlackBeltNinja = new Unit("Black Belt Ninja", 4,5,4);
+
+const HardAlgorithm = new Effect("Hard Algorithm", 2, "resilience", 3, "increase target's resilience by 3");
+const promise = new Effect("Unhandled Promise Rejection", 1, "resilience", -2, "reduce target's resilience by 2");
+const pair = new Effect("Pair Programming", 3, "power", +2, "increase target's power by 2")
+
+HardAlgorithm.play(RedBeltNinja);
+promise.play(RedBeltNinja);
+pair.play(RedBeltNinja);
+RedBeltNinja.attack(BlackBeltNinja);
+
+
 
 
 
